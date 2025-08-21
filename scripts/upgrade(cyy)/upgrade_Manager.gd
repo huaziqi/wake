a7e292959f2570ef2d08fe6691a6337c7591ce17
fanqieCyy <110085617+fanqieCyy@users.noreply.github.com>
2025-08-21 23:05:32 +0800

    upgrade
    升级系统

---
+++ scripts/upgrade(cyy)/upgrade_Manager.gd
@@ -0,0 +1,37 @@
+extends Node
+
+@export var upgrade_pool:Array[AbilityUpgrade]
+@export var dna_manager:Node
+@export var upgrade_scene:PackedScene
+
+var current_upgrades={}
+
+func _ready():
+	dna_manager.level_up.connect(on_level_up)
+	
+	
+	
+func on_level_up(current_level:int):
+	var chosen_upgrade=upgrade_pool.pick_random() as AbilityUpgrade
+	if chosen_upgrade==null:
+		return  
+		
+	var upgrade_screen_instance=upgrade_scene.instantiate()
+	add_child(upgrade_screen_instance) 	
+	upgrade_screen_instance.set_ability_upgrades([chosen_upgrade]as Array[AbilityUpgrade])
+	upgrade_screen_instance.upgrade_selected.connect(on_upgrade_selected)
+	
+	
+func apply_upgrade(upgrade:AbilityUpgrade):
+	var has_upgrades=current_upgrades.has(upgrade.id)
+	
+	if !has_upgrades:
+		current_upgrades[upgrade.id]={
+			"resource"=upgrade,    
+			"quantity"=1
+		}
+	else:
+		current_upgrades["quantity"]+=1
+
+func on_upgrade_selected(upgrade:AbilityUpgrade):
+	apply_upgrade(upgrade)
