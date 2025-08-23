53c0a87a0899c70ac28348508dad4fce65f8f5a4
fanqieCyy <110085617+fanqieCyy@users.noreply.github.com>
2025-08-23 10:11:15 +0800

    升级系统1.1
    修复了big，目前已经可以正常运作

--- scripts/upgrade(cyy)/dnaManager.gd
+++ scripts/upgrade(cyy)/dnaManager.gd
@@ -3,16 +3,16 @@ extends Node
 
 signal dna_updated(current_dna:float,target_dna:float)
 signal level_up(new_level:int)
-const TARGET_GROWTH=5
+const TARGET_GROWTH=5           #每一级增加的所需经验量
 
 var current_dna=0 
 var current_level=1
-var target_dna=5
+var target_dna=1                  #初始化等级，经验，所需经验量
 
 func _ready():	
-	Gameevent.dna_collected.connect(on_dna_collected)
+	Gameevent.dna_collected.connect(on_dna_collected)     
 	
-func increment_dna(number:float):
+func increment_dna(number:float):                       #   更新经验量，如果升级则增加所需的经验量，这个函数主要是处理函数
 	current_dna=min(current_dna+number,target_dna)
 
 	dna_updated.emit(current_dna,target_dna)
@@ -25,7 +25,7 @@ func increment_dna(number:float):
 
 
 
-func on_dna_collected(number:float):
+func on_dna_collected(number:float):         
 	increment_dna(number)
 
 	
