31d2cda56537f9b3f045eada8627010c2d38875c
fanqieCyy <110085617+fanqieCyy@users.noreply.github.com>
2025-09-01 00:20:41 +0800

    我独自升级1.5
    多样化

--- scripts/upgrade(cyy)/upgrade_Manager.gd
+++ scripts/upgrade(cyy)/upgrade_Manager.gd
@@ -9,16 +9,22 @@ var current_upgrades={} #储存已经升级的相关所有数据
 func _ready():
 	dna_manager.level_up.connect(on_level_up)       
 	
-	
-	
-func on_level_up(current_level:int):
-	var chosen_upgrade=upgrade_pool.pick_random() as AbilityUpgrade
-	if chosen_upgrade==null:                                     #随机生成能力卡牌
-		return  
+
+func pick_upgrades():
+	var chosen_upgrades:Array[AbilityUpgrade]=[]#用实现卡牌互不相同
+	var filtered_upgrades=upgrade_pool.duplicate()
+	for i in 2:
+		var chosen_upgrade=filtered_upgrades.pick_random() as AbilityUpgrade   #如果返回true则保留括号内的参数                                #随机生成能力卡牌
+		chosen_upgrades.append(chosen_upgrade)
+		filtered_upgrades=filtered_upgrades.filter(func(upgrade): return upgrade.id!=chosen_upgrade.id) 
+	return chosen_upgrades	
 		
+	
+func on_level_up(current_level:int): 
 	var upgrade_screen_instance=upgrade_scene.instantiate()         #将升级界面实例化（场景化）
 	add_child(upgrade_screen_instance) 	        #加入到子节点
-	upgrade_screen_instance.set_ability_upgrades([chosen_upgrade]as Array[AbilityUpgrade])       #
+	var chosen_upgrades=pick_upgrades()
+	upgrade_screen_instance.set_ability_upgrades(chosen_upgrades as Array[AbilityUpgrade])       #
 	upgrade_screen_instance.upgrade_selected.connect(on_upgrade_selected)           #在upgrade_screen里有相关介绍
 	
 	
