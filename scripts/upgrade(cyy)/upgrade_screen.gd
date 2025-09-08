1eacc2fdd7321a339311a0f1a00a2e70e41674d1
fanqieCyy <110085617+fanqieCyy@users.noreply.github.com>
2025-09-08 13:18:25 +0800

    升级系统1.82
    增加小动画

--- scripts/upgrade(cyy)/upgrade_screen.gd
+++ scripts/upgrade(cyy)/upgrade_screen.gd
@@ -9,12 +9,15 @@ func _ready():
 	get_tree().paused=true        #卡牌出现时，全场暂停
 
 func set_ability_upgrades(upgrades:Array[AbilityUpgrade]):  #实例化卡牌
+	var delay=0#卡片生成的时间差
 	for upgrade in upgrades:
 		var card_instance=upgrade_card_scene.instantiate() #实例化卡牌
 		card_container.add_child(card_instance)
 		card_instance.set_ability_upgrade(upgrade)       #设置数值
+		card_instance.play_in(delay)	
 		card_instance.selected.connect(on_upgrade_selected.bind(upgrade)) #bind应该是把参数传到前面的函数，应该
-
+		delay+=0.15
+		
 func on_upgrade_selected(upgrade:AbilityUpgrade):        
 	upgrade_selected.emit(upgrade)                  #发出”玩家选择的能力卡牌“这一信号
 	get_tree().paused=false   #选择卡牌后时间正常流逝
